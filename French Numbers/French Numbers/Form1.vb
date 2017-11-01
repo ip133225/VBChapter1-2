b4e3823c04cff764896f8f2bbc89eb2e26ee4113
ip133225 <ip133225@students.davenport.k12.ia.us>
2017-11-01 10:28:58 -0500

    French Nunmbers

---
+++ French Numbers/French Numbers/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        MessageBox.Show("Un")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        MessageBox.Show("quatre")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        MessageBox.Show("cinq")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        MessageBox.Show("Sortie")
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        MessageBox.Show("deux")
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        MessageBox.Show("trois")
+    End Sub
+End Class
