e05060161ea33bd77adb13e949bf08bd78377e9a
ip133225 <ip133225@students.davenport.k12.ia.us>
2017-11-02 10:43:22 -0500

    Page-107-Number-2

---
+++ Page-107-Number-2/Page-107-Number-2/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class NameAndAdress
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblStreet = New System.Windows.Forms.Label()
+        Me.lblCityStateZip = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(54, 201)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(75, 23)
+        Me.btnOne.TabIndex = 0
+        Me.btnOne.Text = "Show Info"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(144, 201)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 1
+        Me.btnTwo.Text = "Exit"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(82, 37)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(103, 19)
+        Me.lblName.TabIndex = 2
+        Me.lblName.Text = "William Wolfe"
+        Me.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblStreet
+        '
+        Me.lblStreet.AutoSize = True
+        Me.lblStreet.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStreet.Location = New System.Drawing.Point(54, 70)
+        Me.lblStreet.Name = "lblStreet"
+        Me.lblStreet.Size = New System.Drawing.Size(160, 19)
+        Me.lblStreet.TabIndex = 3
+        Me.lblStreet.Text = "888 South Main Street"
+        Me.lblStreet.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCityStateZip
+        '
+        Me.lblCityStateZip.AutoSize = True
+        Me.lblCityStateZip.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCityStateZip.Location = New System.Drawing.Point(54, 104)
+        Me.lblCityStateZip.Name = "lblCityStateZip"
+        Me.lblCityStateZip.Size = New System.Drawing.Size(165, 19)
+        Me.lblCityStateZip.TabIndex = 4
+        Me.lblCityStateZip.Text = "Waynesville, NC 28786"
+        Me.lblCityStateZip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'NameAndAdress
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblCityStateZip)
+        Me.Controls.Add(Me.lblStreet)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Name = "NameAndAdress"
+        Me.Text = "Name and Adress"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnOne As System.Windows.Forms.Button
+    Friend WithEvents btnTwo As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblStreet As System.Windows.Forms.Label
+    Friend WithEvents lblCityStateZip As System.Windows.Forms.Label
+
+End Class
---
+++ Page-107-Number-2/Page-107-Number-2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class NameAndAdress
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblStreet.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
